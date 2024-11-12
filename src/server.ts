828da6fac6fc9e30e9d4eb3bd519f2280e70ceb4
pranshul <pranshul.evdtechnology@gmail.com>
2024-11-12 23:13:51 +0530

    initial

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const port = 5001;
+
+app.get("/", (req, res) => {
+  res.send("hello, world");
+});
+
+app.listen(port, () => {
+  console.log("server listening on port" + port);
+});
