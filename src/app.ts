584bafed9f3fb9fa21f9f09dfca000e1808507e4
pranshul <pranshul.evdtechnology@gmail.com>
2024-11-14 23:07:55 +0530

    api(post)&refactor

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,11 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
-
+import notesRoutes from "./routes/notes";
 const app = express();
 
-app.get("/", async (req, res, next) => {
-  try {
-    const notes = await NoteModel.find().exec();
-    res.status(200).json(notes);
-  } catch (err) {
-    console.log("err", err);
-    next(err);
-  }
-});
+app.use(express.json());
 
+app.use("/api/notes", notesRoutes);
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
---
+++ src/controllers/notes.ts
@@ -0,0 +1,24 @@
+import { RequestHandler } from "express";
+import NoteModel from "../models/note";
+
+export const getNotes: RequestHandler = async (req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (err) {
+    next(err);
+  }
+};
+export const createNote: RequestHandler = async (req, res, next) => {
+  const title = req.body.title;
+  const text = req.body.text;
+  try {
+    const newNote = await NoteModel.create({
+      title: title,
+      text: text,
+    });
+    res.status(201).json(newNote);
+  } catch (err) {
+    next(err);
+  }
+};
